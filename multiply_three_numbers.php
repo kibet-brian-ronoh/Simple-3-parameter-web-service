43bb1a5709037fdacb590efb5a4e59eebdc6682c
yobra87 <48868025+yobra87@users.noreply.github.com>
2019-10-01 00:51:13 +0300

    Add files via upload
    Multiplies three numbers with a http url call

---
+++ multiply_three_numbers.php
@@ -0,0 +1,11 @@
+<?php
+
+$first_number =$_GET['number1'];
+$second_number = $_GET['number2'];
+$third_number = $_GET['number3'];
+
+$ans= $first_number * $second_number * $third_number;
+
+echo ($ans);
+
+?>
